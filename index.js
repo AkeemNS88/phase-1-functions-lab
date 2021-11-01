a2baa6d65a14250ef48d84613ea80ff48a13df8f
Akeem <AkeemS11788@gmail.com>
2021-11-01 18:50:47 -0400

    commit tree c04d0f313861c65a4dfbdfbd1955687b28af9b13

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    if (location < 42) {
+    return 42 - location
+    };
+    return location - 42
+}
+
+function distanceFromHqInFeet(location) {
+    return distanceFromHqInBlocks(location) * 264
+}
+
+function distanceTravelledInFeet(block, block2) {
+    if (block < block2) {
+        return (block2 - block) * 264
+    };
+    return (block - block2) * 264
+}
+
+function calculatesFarePrice(block, block2) {
+    let stepsTravelled = Math.abs(block - block2) * 264
+    if (stepsTravelled <= 400) { return stepsTravelled * 0}
+    else if(stepsTravelled > 400 && stepsTravelled < 2000) { return (stepsTravelled - 400) * .02}
+    else if(stepsTravelled >= 2000 && stepsTravelled < 2500) {return 25}
+    else if(stepsTravelled > 2500) {return 'cannot travel that far'}
+}
